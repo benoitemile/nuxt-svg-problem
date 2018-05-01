384c8dbbef4cce8945fea93ec543fae236b8cb0b
Benoît Emile <bemile@maisonsdumonde.com>
2018-05-01 19:49:54 +0200

    done with 2 layouts / 2 pages

--- middleware/ismobile.js
+++
@@ -1,9 +0,0 @@
-// middleware/isMobile.js
-import MobileDetect from 'mobile-detect';
-
-export default function (context) {
-  console.log('middleware');
-  const userAgent = process.server ? context.req.headers['user-agent'] : window.navigator.userAgent;
-  const detect = new MobileDetect(userAgent);
-  context.isMobile = detect.mobile();
-}
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <pre>{{ isMobile }}</pre>
+    <pre>{{ component }}</pre>
     <component :is="component"/>
   </div>
 </template>
@@ -10,35 +12,35 @@ import MobileDetect from 'mobile-detect';
 const mobile = () => import('~/components/pages/home/mobile.vue');
 const desktop = () => import('~/components/pages/home/desktop.vue');
 
+function checkUserAgent(context) {
+  const userAgent = process.server ? context.req.headers['user-agent'] : window.navigator.userAgent;
+  const detect = new MobileDetect(userAgent);
+  return detect.mobile();
+}
+
 export default {
-  middleware: 'ismobile',
   data() {
     return {
-      isMobile: null,
+      isMobile() {},
       component() {}, // The result from asyncData will be merged with data
     }
   },
   async asyncData(context) {
     return {
-      isMobile: context.isMobile,
-      component: context.isMobile ? mobile : desktop,
+      isMobile: checkUserAgent(context) ? 'mobile' : 'desktop',
+      component: checkUserAgent(context) ? mobile : desktop,
     }
   },
   layout(context) {
-    console.log('layout');
-    const userAgent = process.server ? context.req.headers['user-agent'] : window.navigator.userAgent;
-    const detect = new MobileDetect(userAgent);
-    let isMobile = detect.mobile();
+    const isMobile = checkUserAgent(context);
     return isMobile ? 'mobile' : 'desktop'; // switching between layouts/mobile and layouts/desktop
   },
   methods: {
     handleWindowResize() {
-      this.component =  this.checkSize() ? mobile : desktop;
+      this.isMobile = this.checkWindowSize() ? 'mobile' : 'desktop',
+      this.component = this.checkWindowSize() ? mobile : desktop;
     },
-    checkSize() {
-      if (!window) {
-        return false;
-      }
+    checkWindowSize() {
       let isMobile;
       if (window.screen.availWidth < 768) {
         isMobile = true;
